106c3900670ffcc4beae8762ca07d23bea72a8e6
Alan Ponte <alanjponte@gmail.com>
2025-10-30 21:27:43 -0700

    Add secrets manager - add fast api fixture

--- tests/test_routers/test_health.py
+++ tests/test_routers/test_health.py
@@ -3,7 +3,7 @@
 HEALTH_URI = "/health/"
 
 
-def test_health_ok(httpClient):
-    resp = httpClient.get(HEALTH_URI)
+def test_health_ok(app_client):
+    resp = app_client.get(HEALTH_URI)
     assert resp.status_code == 200
     assert resp.json() == {"status": "ok"}
